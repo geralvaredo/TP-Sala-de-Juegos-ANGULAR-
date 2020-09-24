4fc4fce9b2ef9ec35bc4e0024f1ef788fe64e117
German Alvaredo <geralvaredo@github.com>
2020-09-24 18:11:36 -0300

    Pantalla de piedra papel o tijera

--- src/app/componentes/piedra-papel-tijera/piedra-papel-tijera.component.ts
+++ src/app/componentes/piedra-papel-tijera/piedra-papel-tijera.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {JuegoPiedraPapelTijera} from '../../clases/juego-piedra-papel-tijera';
 
 @Component({
   selector: 'app-piedra-papel-tijera',
@@ -7,9 +8,89 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PiedraPapelTijeraComponent implements OnInit {
 
-  constructor() { }
+  mensaje: string ;
+  mostrar: boolean ;
+  resultados : boolean;
+  ppt : JuegoPiedraPapelTijera;
+  imagenEleccion : string ;
+  imagenRival : string ;
+  imgRock =  './assets/imagenes/rock.jpeg' ;
+  imgPaper = './assets/imagenes/paper.jpeg' ;
+  imgScissor = './assets/imagenes/scissors.jpeg';
+  imgVersus = './assets/imagenes/versusChico.jpeg';
+
+
+  constructor() {
+    this.mostrar = true;
+    this.resultados = false;
+    this.ppt = new JuegoPiedraPapelTijera();
+  }
 
   ngOnInit() {
   }
+
+   jugar(){
+     this.resultados = false;
+     this.mostrar = false;
+    this.mensaje = "Elija una de las opciones";
+   }
+
+  opcion(event: Event){
+
+      switch (event.srcElement.id){
+        case 'rock': this.ppt.eleccion = 0; break;
+        case 'paper': this.ppt.eleccion = 1; break;
+        case 'scissor': this.ppt.eleccion = 2; break;
+      }
+      this.opcionRival();
+      this.obtenerGanador();
+      this.resultadosEnPantalla();
+
+   }
+
+   opcionRival(){
+        this.ppt.rival = Math.floor( Math.random() * 3 ) ;
+   }
+
+   obtenerGanador(){
+     this.ppt.verificacionResultado();
+     console.log("mi jugada: " + this.ppt.eleccion);
+     console.log("jugada Rival: " + this.ppt.rival);
+     console.log("mensaje" + this.ppt.mensaje);
+
+   }
+
+
+   resultadosEnPantalla(){
+     this.mostrar = true;
+     this.resultados = true ;
+     this.imgEleccion();
+     this.imgRival();
+     this.mensaje = this.ppt.mensaje;
+
+   }
+
+   imgEleccion(){
+     switch (this.ppt.eleccion){
+       case 0: this.imagenEleccion = this.imgRock ; break;
+       case 1: this.imagenEleccion = this.imgPaper ; break;
+       case 2: this.imagenEleccion = this.imgScissor ; break;
+     }
+   }
+
+   imgRival(){
+     switch (this.ppt.rival){
+       case 0: this.imagenRival = this.imgRock ; break;
+       case 1: this.imagenRival = this.imgPaper ; break;
+       case 2: this.imagenRival = this.imgScissor ; break;
+     }
+   }
+
+
+
+
+
+
+
 
 }
