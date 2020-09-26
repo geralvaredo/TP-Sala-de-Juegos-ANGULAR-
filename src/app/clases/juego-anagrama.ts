cae85d1e5beb74edfa3654796efbac576755b8dc
German Alvaredo <geralvaredo@github.com>
2020-09-25 22:44:24 -0300

    Juego de Anagrama Completo

--- src/app/clases/juego-anagrama.ts
+++ src/app/clases/juego-anagrama.ts
@@ -1,2 +1,73 @@
-export class JuegoAnagrama {
+import {Juego} from './juego';
+
+export class JuegoAnagrama extends Juego{
+
+  public respuesta:string;
+  public anagrama:string = '';
+  public intentos: number = 0;
+  public counter: number = 0 ;
+  private wordList:string[] = ["silbar", "acuerdo",
+    "integrarla", "noguera", "prisa", "pagar"];
+
+  constructor(gano?: boolean, jugador?: string) {
+    super("Anagrama", gano, jugador);
+
+  }
+
+  verificacionResultado(): boolean {
+    return null;
+  }
+
+  public checkrespuesta(): boolean{
+    switch (this.anagrama) {
+      case 'silbar':
+        if(this.respuesta === 'brasil') {
+          return true;
+        }
+        break;
+      case 'acuerdo':
+        if(this.respuesta === 'ecuador') {
+          return true;
+        }
+        break;
+      case 'integrarla':
+        if(this.respuesta === 'inglaterra') {
+          return true;
+        }
+        break;
+      case 'noguera':
+        if(this.respuesta === 'noruega') {
+          return true;
+        }
+        break;
+      case 'tangerina':
+        if (this.respuesta === 'argentina') {
+          return true;
+        }
+        break;
+      case 'prisa':
+        if(this.respuesta === 'paris') {
+          return true;
+        }
+        break;
+      default:
+        return false;
+    }
+  }
+
+
+
+  public obtenerAnagrama() {
+    this.anagrama = this.wordList[Math.floor(Math.random() * 6)];
+  }
+
+  public verificar(): boolean {
+   return this.checkrespuesta() ;
+  }
+
+  condicion(){
+      this.intentos = this.intentos + 1;
+  }
+
+
 }
--- src/app/componentes/anagrama/anagrama.component.ts
+++ src/app/componentes/anagrama/anagrama.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {JuegoAnagrama} from '../../clases/juego-anagrama';
+import {isBoolean} from 'util';
+import {element} from 'protractor';
 
 @Component({
   selector: 'app-anagrama',
@@ -7,9 +10,104 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnagramaComponent implements OnInit {
 
-  constructor() { }
+  @Output() enviarJuego: EventEmitter<any> = new EventEmitter<any>();
+
+  nuevoJuego: JuegoAnagrama;
+  message: string;
+  palabra: string;
+  hideComponent : boolean;
+  hideTag : boolean ;
+  hideRespuesta: boolean;
+  hideJugar: boolean;
+  resultado : boolean;
+
 
   ngOnInit() {
   }
+
+  constructor() {
+    this.intentos() ;
+  }
+
+  generarPalabra(){
+    this.nuevoJuego.respuesta != "" ?  this.limpiarCampos() : "";
+     this.ocultarAlGenerarPalabra();
+     this.nuevoJuego.obtenerAnagrama();
+    this.palabra = this.nuevoJuego.anagrama;
+
+  }
+
+   ocultarAlGenerarPalabra(){
+     this.hideComponent = true;
+     this.hideJugar = true ;
+     this.hideRespuesta = false ;
+   }
+
+
+  public respuesta() {
+    this.nuevoJuego.counter++;
+    console.log("ronda: " + this.nuevoJuego.counter);
+    if(this.nuevoJuego.counter == 5){
+      this.rondaCerrada();
+    }
+    else{
+      this.nuevoJuego.verificar() ? this.resultadoVerificado() : "";
+      this.generarPalabra();
+    }
+
+  }
+
+  resultadoVerificado(){
+    this.nuevoJuego.condicion();
+    console.log(this.nuevoJuego.intentos);
+    this.enviarJuego.emit(this.nuevoJuego);
+    this.nuevoJuego.anagrama = '';
+    this.hideTag = false;
+    this.nuevoJuego.respuesta = "";
+  }
+
+  rondaCerrada(){
+    this.resultado =  this.evaluarRondas();
+    this.resultado ? this.mostrarMensaje("Ganador", this.resultado) :  this.mostrarMensaje("Perdedor", this.resultado);
+   }
+
+   evaluarRondas(){
+   this.nuevoJuego.intentos >= 3 ?  this.nuevoJuego.gano = true : this.nuevoJuego.gano = false;
+   return this.nuevoJuego.gano;
+   }
+
+
+  mostrarMensaje(mensaje:string ,ganador: boolean) {
+    this.message = mensaje;
+    var x = document.getElementById("snackbar");
+    ganador ? x.className = "neon-verde" :  x.className = "neon-rojo";
+    var modelo=this;
+    setTimeout(function(){
+      x.className = x.className.replace("show", "");
+       modelo.intentos();
+    }, 500);
+
+  }
+
+   intentos(){
+     this.nuevoJuego = new JuegoAnagrama();
+     this.hideComponent = false;
+     this.hideJugar = false ;
+     this.hideTag = true;
+     this.hideRespuesta = true;
+   }
+
+   limpiarCampos(){
+     this.nuevoJuego.respuesta = "";
+     this.nuevoJuego.gano = null;
+     var x = document.getElementById("snackbar");
+     x.className = "";
+     this.message = "";
+   }
+
+
+
+
+
 
 }
