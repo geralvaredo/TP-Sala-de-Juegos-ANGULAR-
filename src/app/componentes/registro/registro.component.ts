fda38f253b6b720724634fe77aa1f700087422f9
German Alvaredo <geralvaredo@github.com>
2020-09-29 10:17:57 -0300

    Componente Registro

--- src/app/componentes/registro/registro.component.ts
+++ src/app/componentes/registro/registro.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import {Usuario} from "../../clases/usuario";
+import {AuthService} from "../../servicios/auth.service";
 //para poder hacer las validaciones
 //import { Validators, FormBuilder, FormControl, FormGroup} from '@angular/forms';
 @Component({
@@ -9,14 +11,45 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 })
 export class RegistroComponent implements OnInit {
 
- /* constructor( private miConstructor:FormBuilder) { }
-  email=new FormControl('',[Validators.email]);
-  formRegistro:FormGroup=this.miConstructor.group({
-    usuario:this.email
-  });*/
-  constructor( ) { }
+  isRegistered = 'Juegos';
+  registerError = 'Error';
+  usuario = new Usuario();
+  ErrorOnRegister = "ErrorOnRegister";
+
+  constructor(private auth: AuthService ) { }
 
   ngOnInit() {
   }
+
+  cancelar(){
+    this.auth.redirect('Principal');
+  }
+
+
+  async onRegister(): Promise<void> {
+    try {
+      const user = await this.auth.register(this.usuario);
+      if (user) {
+        // this.mensajeDireccion(this.usuario);
+        this.usuario._email = "";
+        this.usuario._pass = "";
+        this.auth.redirect(this.isRegistered);
+      } else {
+        this.auth.redirect(this.registerError);
+      }
+    } catch (error) {
+      console.log(this.ErrorOnRegister, error);
+    }
+  }
+
+
+
+     mensajeDireccion(usuario: Usuario){
+    setTimeout(function(){
+      alert("Bievenido" + usuario._email);
+    }, 2000);
+
+  }
+
 
 }
