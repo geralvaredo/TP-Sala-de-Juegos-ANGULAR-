8e2506b6698dd45a06bf6a1c67397a276f8011fb
German Alvaredo <geralvaredo@github.com>
2020-09-29 10:17:44 -0300

    Componente login

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 import {Subscription} from "rxjs";
 import {TimerObservable} from "rxjs/observable/TimerObservable";
+import {AuthService} from "../../servicios/auth.service";
+import {Usuario} from "../../clases/usuario";
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -11,16 +13,19 @@ import {TimerObservable} from "rxjs/observable/TimerObservable";
 export class LoginComponent implements OnInit {
 
   private subscription: Subscription;
-  usuario = '';
+  usuario = new Usuario();
+  isLogin = 'Principal';
+  notLogin = 'Error';
+  errorLogin = 'ErrorOnlogin->';
   clave= '';
   progreso: number;
-  progresoMensaje="esperando..."; 
+  progresoMensaje="esperando...";
   logeando=true;
   ProgresoDeAncho:string;
+  isFirstOpen = true;
 
-  clase="progress-bar progress-bar-info progress-bar-striped ";
 
-  constructor(
+  constructor(private auth: AuthService,
     private route: ActivatedRoute,
     private router: Router) {
       this.progreso=0;
@@ -31,51 +36,31 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
 
-  Entrar() {
-    if (this.usuario === 'admin' && this.clave === 'admin') {
-      this.router.navigate(['/Principal']);
+  cancelar(){
+    this.auth.redirect('Principal');
+  }
+
+  registro(){
+    this.auth.redirect('Registro');
+  }
+
+  administrador(){
+    this.usuario._email = "admin@admin.com";
+    this.usuario._pass = "admin123";
+  }
+
+  async onLogin(): Promise<void> {
+    try {
+      const logging = await this.auth.login(this.usuario);
+      if (logging) {
+        this.auth.redirect(this.isLogin);
+      } else {
+        this.auth.redirect(this.notLogin);
+      }
+    } catch (error) {
+      console.log(this.errorLogin, error);
     }
   }
-  MoverBarraDeProgreso() {
-    
-    this.logeando=false;
-    this.clase="progress-bar progress-bar-danger progress-bar-striped active";
-    this.progresoMensaje="NSA spy..."; 
-    let timer = TimerObservable.create(200, 50);
-    this.subscription = timer.subscribe(t => {
-      console.log("inicio");
-      this.progreso=this.progreso+1;
-      this.ProgresoDeAncho=this.progreso+20+"%";
-      switch (this.progreso) {
-        case 15:
-        this.clase="progress-bar progress-bar-warning progress-bar-striped active";
-        this.progresoMensaje="Verificando ADN..."; 
-          break;
-        case 30:
-          this.clase="progress-bar progress-bar-Info progress-bar-striped active";
-          this.progresoMensaje="Adjustando encriptación.."; 
-          break;
-          case 60:
-          this.clase="progress-bar progress-bar-success progress-bar-striped active";
-          this.progresoMensaje="Recompilando Info del dispositivo..";
-          break;
-          case 75:
-          this.clase="progress-bar progress-bar-success progress-bar-striped active";
-          this.progresoMensaje="Recompilando claves facebook, gmail, chats..";
-          break;
-          case 85:
-          this.clase="progress-bar progress-bar-success progress-bar-striped active";
-          this.progresoMensaje="Instalando KeyLogger..";
-          break;
-          
-        case 100:
-          console.log("final");
-          this.subscription.unsubscribe();
-          this.Entrar();
-          break;
-      }     
-    });
-    //this.logeando=true;
-  }
+
 
 }
