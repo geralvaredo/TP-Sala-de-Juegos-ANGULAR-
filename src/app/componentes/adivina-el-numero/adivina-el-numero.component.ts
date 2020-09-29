f61bd4449415b479079b7fdb5cc8569ae66862f1
German Alvaredo <geralvaredo@github.com>
2020-09-29 16:38:08 -0300

    Persistencia para adivina el numero

--- src/app/componentes/adivina-el-numero/adivina-el-numero.component.ts
+++ src/app/componentes/adivina-el-numero/adivina-el-numero.component.ts
@@ -1,6 +1,7 @@
 
-import { Component, OnInit ,Input,Output,EventEmitter} from '@angular/core';
+import { Component, OnInit ,Output,EventEmitter} from '@angular/core';
 import { JuegoAdivina } from '../../clases/juego-adivina'
+import {PersistenceService} from "../../servicios/persistence.service";
 
 @Component({
   selector: 'app-adivina-el-numero',
@@ -8,24 +9,39 @@ import { JuegoAdivina } from '../../clases/juego-adivina'
   styleUrls: ['./adivina-el-numero.component.css']
 })
 export class AdivinaElNumeroComponent implements OnInit {
- @Output() enviarJuego: EventEmitter<any>= new EventEmitter<any>();
 
-  nuevoJuego: JuegoAdivina;
+ @Output() enviarJuego: EventEmitter<any>= new EventEmitter<any>();
+  adivina: JuegoAdivina;
   Mensajes:string;
   contador:number;
+  puntaje: number;
   ocultarVerificar:boolean;
   jugar : boolean;
+  nombreJugador: string ;
 
-  constructor() {
-    this.nuevoJuego = new JuegoAdivina();
+
+  constructor(private db: PersistenceService){
+
+    this.adivina = new JuegoAdivina();
     this.ocultarVerificar=true;
     this.jugar = false;
+    this.contador = 0;
+    this.puntaje = 0;
   }
+
+
+  ngOnInit() {
+  }
+
+  obtenerJugador(){
+    return this.nombreJugador = sessionStorage.getItem('usuario');
+  }
+
   generarnumero() {
-    if(this.nuevoJuego.numeroIngresado !== undefined){
-      this.nuevoJuego.numeroIngresado = null ;
+    if(this.adivina.numeroIngresado !== undefined){
+      this.adivina.numeroIngresado = null ;
     }
-    this.nuevoJuego.generarnumero();
+    this.adivina.generarnumero();
     this.contador=0;
     this.jugar = true;
     this.ocultarVerificar = false;
@@ -34,62 +50,55 @@ export class AdivinaElNumeroComponent implements OnInit {
     this.jugar = false;
     this.contador++;
     this.ocultarVerificar=true;
-    console.info("numero Secreto:",this.nuevoJuego.gano);
-    if (this.nuevoJuego.verificacionResultado()){
-      this.enviarJuego.emit(this.nuevoJuego);
-      this.MostarMensaje("Sos un Genio!!!",true);
-      this.nuevoJuego.numeroSecreto=0;
-
+    if (this.adivina.verificacionResultado()){
+      if(this.contador == 1){
+          this.puntaje = 10;
+      }
+      console.log(this.adivina);
+      this.datosDeLaPartida( this.contador,this.puntaje,true);
+      this.mostrarMensaje("Sos un Genio!!!",true);
+      this.adivina.numeroSecreto=0;
     }else{
       let mensaje:string;
       switch (this.contador) {
-        case 1:
-          mensaje="No, intento fallido, animo";
-          break;
-          case 2:
-          mensaje="No,Te estaras Acercando???";
-          break;
-          case 3:
-          mensaje="No es, Yo crei que la tercera era la vencida.";
-          break;
-          case 4:
-          mensaje="No era el  "+this.nuevoJuego.numeroIngresado;
-          break;
-          case 5:
-          mensaje=" intentos y nada.";
-          break;
-          case 6:
-          mensaje="Afortunado en el amor";
-          break;
-
-        default:
-            mensaje="Ya le erraste "+ this.contador+" veces";
-          break;
+          case 1:  mensaje="No, intento fallido, animo";  break;
+          case 2:  mensaje="No,Te estaras Acercando???";  break;
+          case 3:  mensaje="No es, Yo crei que la tercera era la vencida.";  break;
+          case 4:  mensaje="No era el  "+this.adivina.numeroIngresado;   break;
+          case 5:  mensaje=" intentos y nada."; break;
+          case 6:  mensaje="Afortunado en el amor"; break;
+        default:   mensaje="Ya le erraste "+ this.contador+" veces"; break;
       }
-      this.MostarMensaje("#"+this.contador+" "+mensaje+" ayuda :"+this.nuevoJuego.retornarAyuda());
-
+      this.puntaje = 1;
+      this.datosDeLaPartida(this.contador,this.puntaje,false);
+      this.mostrarMensaje(this.contador+" "+mensaje+" ayuda :"+this.adivina.retornarAyuda());
 
     }
-    console.info("numero Secreto:",this.nuevoJuego.gano);
+
+    console.info("numero Secreto:",this.adivina.gano);
   }
 
-  MostarMensaje(mensaje:string="este es el mensaje",ganador:boolean=false) {
+
+  datosDeLaPartida( intentos: number , puntaje: number , partida: boolean ){
+    this.adivina.jugador = this.obtenerJugador();
+    this.adivina.gano = partida;
+    this.adivina.puntaje = puntaje;
+    this.adivina.intentos = intentos;
+    this.db.crearJuego(this.adivina);
+    this.enviarJuego.emit(this.adivina);
+  }
+
+
+
+  mostrarMensaje(mensaje:string="este es el mensaje",ganador:boolean=false) {
     this.Mensajes=mensaje;
-    var x = document.getElementById("snackbar");
-    if(ganador){
-        x.className = "show Ganador";
-      }else{
-        x.className = "show Perdedor";
-      }
-    var modelo=this;
+    let x = document.getElementById("snackbar");
+    (ganador) ?  x.className = "show Ganador" :  x.className = "show Perdedor";
     setTimeout(function(){
       x.className = x.className.replace("show", "");
-      //modelo.ocultarVerificar=false;
      }, 3000);
-    console.info("objeto",x);
 
    }
-  ngOnInit() {
-  }
+
 
 }
