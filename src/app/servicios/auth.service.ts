691b786b3548d699ae11d5512dc0067a46666481
German Alvaredo <geralvaredo@github.com>
2020-09-29 10:17:17 -0300

    Servicio de Autenticacion

---
+++ src/app/servicios/auth.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import {Router} from "@angular/router";
+import {Usuario} from "../clases/usuario";
+import {AngularFireAuth} from "@angular/fire/auth";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  errorLogin = 'ErrorLogin ->';
+  errorVerificationEmailFireBase = 'ErrorVerificationEmailFireBase->';
+  errorRegisterFireBase = 'ErrorRegisterFireBase ->';
+  paginaLogin = '';
+  logOutError = 'error';
+
+
+  constructor(private afAuth: AngularFireAuth, private  router: Router) {
+  }
+
+
+
+  async login(usuario: Usuario): Promise< any> {
+    try {
+       const {user} = await this.afAuth.auth.signInWithEmailAndPassword(usuario._email, usuario._pass);
+       return user;
+    } catch (error) {
+      console.log(this.errorLogin, error);
+    }
+  }
+
+
+
+
+  async register(usuario: Usuario): Promise<any> {
+    try {
+      const {user} = await this.afAuth.auth.createUserWithEmailAndPassword(usuario._email, usuario._pass);
+      await this.verificationEmailFirebase();
+      return user;
+    } catch (error) {
+      console.log(this.errorRegisterFireBase, error);
+    }
+  }
+
+
+  async verificationEmailFirebase(): Promise<void> {
+    try {
+      return (await this.afAuth.auth.currentUser).sendEmailVerification();
+    } catch (error) {
+      console.log(this.errorVerificationEmailFireBase, error);
+    }
+  }
+
+
+  async logOut(): Promise<void> {
+    await this.afAuth.auth.signOut().then(res => {
+      this.redirect(this.paginaLogin);
+    }).catch(error => {
+      console.log(error);
+      this.redirect(this.logOutError);
+    });
+
+  }
+
+  public redirect(router: string): void {
+    this.router.navigate([router]);
+  }
+}
