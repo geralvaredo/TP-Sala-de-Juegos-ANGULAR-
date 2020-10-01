baba3991204c4a7ad4099fa387c7edabb3e830d8
German Alvaredo <geralvaredo@github.com>
2020-10-01 04:31:34 -0300

    funcionacionales de juegos y jugadores en el servicio

--- src/app/servicios/persistence.service.ts
+++ src/app/servicios/persistence.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import {AngularFirestore} from "@angular/fire/firestore";
-import {Timestamp} from "rxjs";
 import {Juego} from "../clases/juego";
+import * as firebase from "firebase";
+import {Jugador} from "../clases/jugador";
 
 
 
@@ -15,20 +16,37 @@ export class PersistenceService {
 
   public crearJuego( juego: Juego ){
      return this.firestore.collection('juegos').add({
-        juego: juego.nombre ,
+        nombre: juego.nombre ,
        jugador: juego.jugador,
        resultado : juego.gano ,
        intentos : juego.intentos,
        puntaje: juego.puntaje,
-       fecha: new Date()
+       fecha: firebase.firestore.Timestamp.now().toDate()
      }).catch(error => {
-         console.log("Error al agregar una coleccion", error);
+         console.log("Error al agregar una coleccion de juegos", error);
      });
 
   }
 
-  public obtenerJuego(){
-    return this.firestore.collection('juegos').get().subscribe();
+
+  public obtenerJuego(juegos : string){
+     return  this.firestore.collection(juegos).valueChanges();
+  }
+
+  public crearJugadores(jugador : Jugador){
+    return this.firestore.collection('jugadores').add({
+      nombre : jugador.nombre ,
+      apodo : jugador.apodo ,
+      avatar : jugador.avatar ,
+      sexo  :   jugador.sexo ,
+      terminos : jugador.terminos
+    }).catch(error => {
+      console.log("Error al agregar una coleccion de jugadores", error);
+    });
+  }
+
+  public obtenerJugadores(jugador: string){
+    return  this.firestore.collection(jugador).valueChanges();
   }
 
 
