35fc62b6a3e191c3ef187256b7b1fb48889dfcd3
German Alvaredo <geralvaredo@github.com>
2020-10-01 04:30:26 -0300

    Lista de Jugadores completa

--- src/app/componentes/jugadores-listado/jugadores-listado.component.ts
+++ src/app/componentes/jugadores-listado/jugadores-listado.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { JugadoresService } from '../../servicios/jugadores.service';
+import {Jugador} from "../../clases/jugador";
+import {PersistenceService} from "../../servicios/persistence.service";
+import {Juego} from "../../clases/juego";
 @Component({
   selector: 'app-jugadores-listado',
   templateUrl: './jugadores-listado.component.html',
@@ -7,20 +10,40 @@ import { JugadoresService } from '../../servicios/jugadores.service';
 })
 export class JugadoresListadoComponent implements OnInit {
 
-  listado:any
-  miJugadoresServicio:JugadoresService
-  
-    constructor(serviceJugadores:JugadoresService) {
-      this.miJugadoresServicio = serviceJugadores;
-      
+  jugadores: Array<Jugador> = [] ;
+
+    constructor(private db: PersistenceService) {
+     this.listarJugadores();
     }
-    
+
+  listarJugadores(){
+    this.db.obtenerJugadores("jugadores").subscribe(
+      (lista: Array<Jugador>) => {
+        for (let i = 0; i < lista.length; i++) {
+             this.jugadores.push(new Jugador(lista[i].nombre,lista[i].apodo, this.pasarImagenes(lista[i].avatar) ,lista[i].sexo));
+             console.log(this.jugadores);
+        }
+      }
+    )
+
+
+
+  }
+
+  pasarImagenes(img : string){
+     switch (img){
+       case 'b': return './assets/imagenes/bowser.jpeg'; break;
+       case 'm': return './assets/imagenes/marioCarton.jpeg'; break;
+       case 'p': return './assets/imagenes/peach.png' ; break ;
+       case 'y': return './assets/imagenes/yoshiCarton.jpeg' ; break ;
+     }
+    }
 
 
   ngOnInit() {
   }
 
-
+/*
   TraerTodos(){
     //alert("totos");
     this.miJugadoresServicio.traertodos('jugadores/','todos').then(data=>{
@@ -43,5 +66,5 @@ export class JugadoresListadoComponent implements OnInit {
 
     })
   }
-
+*/
 }
