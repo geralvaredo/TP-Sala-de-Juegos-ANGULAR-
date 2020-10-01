523afc2332d0cd3962bee002913e91f240a32f4c
German Alvaredo <geralvaredo@github.com>
2020-10-01 04:32:23 -0300

    se modifico el formulario de registro con algunas funcionalidades

--- src/app/componentes/registro/registro.component.ts
+++ src/app/componentes/registro/registro.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import {Usuario} from "../../clases/usuario";
 import {AuthService} from "../../servicios/auth.service";
+import {PersistenceService} from "../../servicios/persistence.service";
+import {Jugador} from "../../clases/jugador";
 //para poder hacer las validaciones
 //import { Validators, FormBuilder, FormControl, FormGroup} from '@angular/forms';
 @Component({
@@ -14,9 +16,17 @@ export class RegistroComponent implements OnInit {
   isRegistered = 'Juegos';
   registerError = 'Error';
   usuario = new Usuario();
+  jugador = new Jugador();
   ErrorOnRegister = "ErrorOnRegister";
+  bowser =  './assets/imagenes/bowser.jpeg' ;
+  mario = './assets/imagenes/marioCarton.jpeg' ;
+  peach = './assets/imagenes/peach.png';
+  yoshi = './assets/imagenes/yoshiCarton.jpeg';
 
-  constructor(private auth: AuthService ) { }
+
+  constructor(private auth: AuthService , private  db: PersistenceService) {
+
+  }
 
   ngOnInit() {
   }
@@ -28,28 +38,48 @@ export class RegistroComponent implements OnInit {
 
   async onRegister(): Promise<void> {
     try {
-      const user = await this.auth.register(this.usuario);
-      if (user) {
-        // this.mensajeDireccion(this.usuario);
-        this.usuario._email = "";
-        this.usuario._pass = "";
-        this.auth.redirect(this.isRegistered);
-      } else {
-        this.auth.redirect(this.registerError);
-      }
+         if( this.usuarioVacio() && this.jugadorVacio()){
+             if(this.contrasenaConRepeticion){
+               const user = await this.auth.register(this.usuario);
+               if (user) {
+                 sessionStorage.setItem("usuario", JSON.stringify(this.usuario._email));
+                 this.jugador.nombre = this.usuario._email;
+                 this.db.crearJugadores(this.jugador);
+                 this.usuario._email = "";
+                 this.usuario._pass = "";
+                 this.auth.redirect(this.isRegistered);
+               } else {
+                 this.auth.redirect(this.registerError);
+               }
+             }
+
+         }
+         else{
+           alert( "Debe completar todos los campos inclusive los terminos y condiciones");
+         }
+
     } catch (error) {
       console.log(this.ErrorOnRegister, error);
     }
   }
 
+   validacion(){
+   }
 
+   usuarioVacio(){
+       return (this.usuario._email !== null && this.usuario._pass !== null) ;
+   }
 
-     mensajeDireccion(usuario: Usuario){
-    setTimeout(function(){
-      alert("Bievenido" + usuario._email);
-    }, 2000);
+   jugadorVacio(){
+    return (this.jugador.apodo !== null && this.jugador.sexo !== null && this.jugador.avatar !== null && this.jugador.terminos !== null) ;
+   }
 
-  }
+   contrasenaConRepeticion(){
+    return (this.usuario._pass == this.usuario._passRepeat);
+   }
 
+   cargaJugador(){
+
+   }
 
 }
